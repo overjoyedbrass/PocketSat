22588be9f61975419a605f665069d202122566ce
overjoyedbrass <riganadam@gmail.com>
2023-04-13 00:25:30 +0200

    default theme added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './styles/main/index.css';
 import App from './components/App';
 
+import { extendTheme, ChakraProvider, ColorModeScript } from "@chakra-ui/react"
+
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+    config: {
+      initialColorMode: "dark",
+      useSystemColorMode: false,
+    },
+    colors: {
+        brand: {
+            100: "#f7fafc",
+            // ...
+            900: "#1a202c",
+        },
+    },
+    styles: {
+        global: (props) => ({
+          body: {
+            bg: "rgb(14, 8, 35)",
+          },
+        }),
+      },
+})
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>      
+        <App />
+    </ChakraProvider>
+    </>
 );
